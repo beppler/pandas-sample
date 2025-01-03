59a7448547ed6ef40412a8a4c3d164fc92f15d0c
Carlos Alberto Costa Beppler <beppler@gmail.com>
2025-01-03 20:45:27 -0300

    Remove deprecation warnings

--- sample.py
+++ sample.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 def main():
     data = pd.read_excel('data.xlsx', na_filter = False)
-    data['valor'].replace('', 0, inplace=True)
-    data['sigla'] = data['nome'].str.split('-', n=2, expand=True)[0]
-    data['nome'].replace('', 'N/I', inplace=True)
-    data['sigla'].replace('', 'NI', inplace=True)
+    data['sigla'] = data['nome'].str.split('-', n=2, expand=True)[0].replace('', 'NI')
+    data['nome'] = data['nome'].replace('', 'N/I')
+    data['valor'] = data['valor'].replace('', '0')
 
     print(data)
 
